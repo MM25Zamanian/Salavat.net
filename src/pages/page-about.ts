d4042d14f2b1847411ab7d5e7772f2178b8b550e
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2021-10-31 16:25:09 +0330

    rtl about page

--- src/pages/page-about.ts
+++ src/pages/page-about.ts
@@ -21,13 +21,19 @@ export class PageAbout extends PageElement {
   render() {
     return html`
       <section>
-        <h1>About</h1>
+        <h1>درباره ما</h1>
 
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
-          delectus? Unde, sit. Fuga modi ea praesentium. Nemo dicta qui, magnam
-          cum dolorum excepturi beatae explicabo quidem fugiat ullam blanditiis
-          minima!
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+          از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+          سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
+          متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه
+          درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با
+          نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+          خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید
+          داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به
+          پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی
+          سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
         </p>
       </section>
     `;
@@ -35,8 +41,8 @@ export class PageAbout extends PageElement {
 
   meta() {
     return {
-      title: 'About',
-      description: 'About page description',
+      title: 'درباره ما',
+      description: 'توضیحات درباره ما',
     };
   }
 }
