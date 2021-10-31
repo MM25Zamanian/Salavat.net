42450e05fda9d18407f592c506a17469758c7d01
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2021-10-31 16:23:36 +0330

    add reboot

---
+++ src/reboot.ts
@@ -0,0 +1,11 @@
+import { css } from 'lit';
+
+export default css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: none;
+    -webkit-tap-highlight-color: transparent;
+  }
+`;
